254f3d0465985e1b49ecca65a60889fb87ae8890
Lyra <81570596+whinee@users.noreply.github.com>
2025-02-12 04:45:44 +0800

    im just fucking around at this point LMFAAOOOO

--- test.py
+++ test.py
@@ -5,25 +5,25 @@ from whines_crossfiledialog import file_dialog
 CrossFileDialog = file_dialog(["pygobject"])
 
 def test():
+    # print(CrossFileDialog.open_file(
+    #     start_dir="~",
+    #     filter={"PDF-Files": "*.pdf", "Python Project": ["*.py", "*.md"]},
+    # ))
     print(CrossFileDialog.open_file(
         start_dir="~",
-        filter={"PDF-Files": "*.pdf", "Python Project": ["*.py", "*.md"]},
+        filter=[{"PDF-Files": "*.pdf"}, ["*.py", "*.txt"], "*.jpg"],
     ))
-    # print(CrossFileDialog.open_file(
-    #     start_dir="~",
-    #     filter=[{"PDF-Files": "*.pdf"}, ["*.py", "*.txt"], "*.jpg"],
-    # ))
-    # print(CrossFileDialog.open_file(
-    #     start_dir="~",
-    #     filter=["*.py", "*.md"],
-    # ))
-    # print(CrossFileDialog.open_file(
-    #     start_dir="~",
-    #     filter="*.py",
-    # ))
-    # print(CrossFileDialog.open_multiple())
-    # print(CrossFileDialog.save_file())
-    # print(CrossFileDialog.choose_folder())
+    print(CrossFileDialog.open_file(
+        start_dir="~",
+        filter=["*.py", "*.md"],
+    ))
+    print(CrossFileDialog.open_file(
+        start_dir="~",
+        filter="*.py",
+    ))
+    print(CrossFileDialog.open_multiple())
+    print(CrossFileDialog.save_file())
+    print(CrossFileDialog.choose_folder())
 
 
 if __name__ == "__main__":
